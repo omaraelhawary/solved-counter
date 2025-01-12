6352dfe577f3b099648918dd5abfcd9699b4d6fc
Omar ElHawary <omaraelhawary@gmail.com>
2025-01-12 22:01:58 +0200

    Apply State

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'dcbc60cfb7a3a39ef7ac');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'e37131ef2b36b8bc0322');
--- src/render.php
+++ src/render.php
@@ -13,6 +13,10 @@
 // Generates a unique id for aria-controls.
 $unique_id = wp_unique_id( 'p-' );
 
+wp_interactivity_state('create-block', array(
+	'solvedCount' => 0,
+));
+
 // Adds the global state.
 wp_interactivity_state(
 	'create-block',
@@ -25,32 +29,6 @@ wp_interactivity_state(
 );
 ?>
 
-<div
-	<?php echo get_block_wrapper_attributes(); ?>
-	data-wp-interactive="create-block"
-	<?php echo wp_interactivity_data_wp_context( array( 'isOpen' => false ) ); ?>
-	data-wp-watch="callbacks.logIsOpen"
-	data-wp-class--dark-theme="state.isDark"
->
-	<button
-		data-wp-on--click="actions.toggleTheme"
-		data-wp-text="state.themeText"
-	></button>
-
-	<button
-		data-wp-on--click="actions.toggleOpen"
-		data-wp-bind--aria-expanded="context.isOpen"
-		aria-controls="<?php echo esc_attr( $unique_id ); ?>"
-	>
-		<?php esc_html_e( 'Toggle', 'solved-counter' ); ?>
-	</button>
-
-	<p
-		id="<?php echo esc_attr( $unique_id ); ?>"
-		data-wp-bind--hidden="!context.isOpen"
-	>
-		<?php
-			esc_html_e( 'Solved Counter - hello from an interactive block!', 'solved-counter' );
-		?>
-	</p>
+<div data-wp-interactive="create-block">
+	<p>Questions Solved: <strong><span data-wp-text="state.solvedCount"></span></strong></p>
 </div>
